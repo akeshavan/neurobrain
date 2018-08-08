979e0708ae9d11e69ef74e89177f290f27052d9d
akeshavan <keshavan@berkeley.edu>
2018-08-08 15:28:32 -0700

    enh: added ami as a dependency

--- src/cat.js
+++ src/cat.js
@@ -1,8 +1,12 @@
 export default class Cat {
   constructor() {
     this._name = 'Cat';
+    this._sound = 'meow';
   }
   get name() {
     return this._name;
+  }
+  get sound() {
+    return this._sound;
   }
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,7 @@
 /* global describe, it, before */
 
 import chai from 'chai';
-import {Cat, Dog} from '../lib/webpack-library-starter.js';
+import {Cat, Dog} from '../lib/neurobrain.js';
 
 chai.expect();
 
