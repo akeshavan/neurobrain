d5f3f8895c071b597371ec874a6f588dc1dbed9a
akeshavan <keshavan@berkeley.edu>
2018-08-08 16:51:06 -0700

    why is this broken

--- src/viewer.js
+++ src/viewer.js
@@ -1,5 +1,25 @@
+import THREE from 'three';
+import { stackHelperFactory } from 'ami.js';
+
+/*
+import CoreUtils from 'base/core/core.utils';
+import LoadersVolume from 'base/loaders/loaders.volume';
+import HelpersStack from 'base/helpers/helpers.stack';
+import HelpersLut from 'base/helpers/helpers.lut';
+import CamerasOrthographic from 'base/cameras/cameras.orthographic';
+import ControlsOrthographic from 'base/controls/controls.trackballortho';
+*/
+
 export default class Viewer {
   constructor(name) {
+    window.THREE = THREE;
+
+    const StackHelper = stackHelperFactory(THREE);
+
+    const sh = StackHelper();
+
+    console.log(sh);
+
     this._name = name;
   }
   get name() {
